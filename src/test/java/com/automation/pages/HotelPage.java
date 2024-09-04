52d8b2a982113b4d4c87bf5fd671516ed62551dd
sreelakshmi-pr <sreepayyoor99@gmail.com>
2024-09-04 11:47:39 +0530

    hotel booking,price sorting,free cancellation sorting added

--- src/test/java/com/automation/pages/HotelPage.java
+++ src/test/java/com/automation/pages/HotelPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import java.security.PublicKey;
-import java.util.List;
+import java.util.*;
 
 
 public class HotelPage extends BasePage {
@@ -162,11 +162,12 @@ public class HotelPage extends BasePage {
 //                }
 //            }while (flag);
         doneBtn.click();
-        }
-        @FindBy(xpath = "//android.widget.Button[@resource-id=\"com.booking:id/button_positive\"]")
-        WebElement okBtn;
-        @FindBy(xpath = "//android.widget.TextView[@text='Please enter your destination']")
-        WebElement errMsg;
+    }
+
+    @FindBy(xpath = "//android.widget.Button[@resource-id=\"com.booking:id/button_positive\"]")
+    WebElement okBtn;
+    @FindBy(xpath = "//android.widget.TextView[@text='Please enter your destination']")
+    WebElement errMsg;
 
     public boolean alertDisplayed() {
         return okBtn.isDisplayed() && errMsg.isDisplayed();
@@ -178,9 +179,9 @@ public class HotelPage extends BasePage {
     WebElement hotelNameOnHotelSearchPage;
 
     public void addHotelToWishlist() {
-        String s=hotelNameOnHotelSearchPage.getAttribute("text");
+        String s = hotelNameOnHotelSearchPage.getAttribute("text");
         System.out.println(s);
-        ConfigReader.setConfigValue("hotel_name",s);
+        ConfigReader.setConfigValue("hotel_name", s);
         wishlistIcon.click();
     }
 
@@ -190,22 +191,253 @@ public class HotelPage extends BasePage {
     WebElement wishlistPageIcon;
     @FindBy(xpath = "//android.widget.ScrollView/android.view.View[4]")
     WebElement clickOnNextTrip;
+
     public void goToWishlistPage() {
-           backBtn.click();
-           wishlistPageIcon.click();
-           clickOnNextTrip.click();
+        backBtn.click();
+        wishlistPageIcon.click();
+        clickOnNextTrip.click();
     }
 
     @FindBy(xpath = "(//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]//android.widget.TextView)[1]")
     WebElement hotelNameOnWishlistPage;
+
     public boolean verifyHotelIsInWishlist() {
-        String a=ConfigReader.getConfigValue("hotel_name");
+        String a = ConfigReader.getConfigValue("hotel_name");
         System.out.println(a);
         return a.equals(hotelNameOnWishlistPage.getAttribute("text"));
     }
 
     public void selectDate() {
         selectDateBtn.click();
+    }
+
+    public void clickOnFirstHotel() {
+        fistHotel.click();
+    }
+
+    @FindBy(xpath = "//android.widget.Button[@resource-id=\"com.booking:id/select_room_cta\"]")
+    WebElement selectRoomBtn;
+
+    public boolean hotelDetailsPage() {
+        return selectRoomBtn.isDisplayed();
+    }
+
+    public void clickOnAvailabilityBtn() {
+        selectRoomBtn.click();
+    }
+
+    @FindBy(xpath = "//android.widget.Button[@text=\"Reserve\"]")
+    WebElement reserveBtn;
+
+    public boolean reservationPage() {
+//        waitForElementToBeClickable(reserveBtn);
+        return reserveBtn.isDisplayed();
+    }
+
+    @FindBy(className = "android.widget.Button")
+    WebElement selectRoom; //select btn
+    @FindBy(xpath = "//androidx.compose.ui.viewinterop.ViewFactoryHolder/android.view.ViewGroup/android.view.View/android.view.View/android.widget.Button")
+    WebElement selectBtn;
+    @FindBy(xpath = "//android.widget.TextView[@text=\"Select and customize\"]")
+    WebElement selectAndCustomise;
+    @FindBy(xpath = "//android.widget.FrameLayout[@resource-id=\"com.booking:id/main_action\"]")
+    WebElement nextStep;
+    @FindBy(xpath = "//android.widget.FrameLayout[@resource-id=\"com.booking:id/room_list_marketing_rewards_card\"]")
+    WebElement oneFullBed;
+    @FindBy(xpath = "//android.widget.FrameLayout[@resource-id=\"com.booking:id/main_action\"]")
+    WebElement nextStep2;
+    @FindBy(xpath = "//android.widget.TextView[@text=\"Confirm\"]")
+    WebElement confirmBtn;
+
+    public void selectAndCustomiseHotel() {
+
+        selectBtn.click();
+
+    }
+
+    public void clickOnReserveBtn() {
+
+
+        try {
+            nextStep.isDisplayed();
+            nextStep.click();
+
+            if (nextStep2.isDisplayed()) {
+                nextStep2.click();
+            }
+            waitForElementToBeVisible(confirmBtn);
+            confirmBtn.click();
+            reserveBtn.click();
+        } catch (Exception e) {
+
+            reserveBtn.click();
+            System.out.println("customisation not exist");
+        }
+    }
+
+    @FindBy(xpath = "//android.widget.TextView[@text=\"Fill in your info\"]")
+    WebElement fillInfoText;
+
+    public boolean fillInfoPage() {
+        return fillInfoText.isDisplayed();
+    }
+
+    @FindBy(xpath = "//android.widget.TextView[@resource-id=\"com.booking:id/bui_input_container_label\" and @text=\"First Name *\"]")
+    WebElement fstName;
+    @FindBy(xpath = "//android.widget.TextView[@resource-id=\"com.booking:id/bui_input_container_label\" and @text=\"Last Name *\"]")
+    WebElement lstName;
+    @FindBy(xpath = "//android.widget.TextView[@resource-id=\"com.booking:id/bui_input_container_label\" and @text=\"Email Address *\"]")
+    WebElement email;
+    @FindBy(xpath = "//android.widget.TextView[@resource-id=\"com.booking:id/bui_input_container_label\" and @text=\"Mobile Phone *\"]")
+    WebElement phoneNum;
+    @FindBy(xpath = "//android.widget.TextView[@text=\"Leisure\"]")
+    WebElement purposeOfTrip;
+
+    public void fillInfo() {
+        fstName.sendKeys(ConfigReader.getConfigValue("first.name"));
+        lstName.sendKeys(ConfigReader.getConfigValue("last.name"));
+        email.sendKeys(ConfigReader.getConfigValue("email"));
+        phoneNum.sendKeys(ConfigReader.getConfigValue("phone.num"));
+        purposeOfTrip.click();
+
+    }
+
+    @FindBy(xpath = "//android.widget.TextView[@text=\"Next step\"]")
+    WebElement nextStep3;
+
+    public void clickOnNextStep() {
+        nextStep3.click();
+    }
+
+    @FindBy(xpath = "//android.widget.TextView[@text=\"Booking Overview\"]")
+    WebElement bookingOverview;
+    @FindBy(xpath = "//androidx.recyclerview.widget.RecyclerView//androidx.compose.ui.viewinterop.ViewFactoryHolder[1]//android.widget.TextView")
+    WebElement hotelNameInOverviewPage;
+
+    public boolean bookingReviewPage() {
+        String s = hotelNameInOverviewPage.getAttribute("text");
+        System.out.println(s);
+        System.out.println(ConfigReader.getConfigValue("hotel_name"));
+        return bookingOverview.isDisplayed() && s.equals(ConfigReader.getConfigValue("hotel_name"));
+
+    }
+
+    @FindBy(xpath = "//android.widget.TextView[@text=\"Sort\"]")
+    WebElement sort;
+
+    public void clickOnSort() {
+        sort.click();
+    }
+
+    @FindBy(xpath = "//android.widget.CheckedTextView[@text=\"Price (low to high)\"]")
+    WebElement priceHighToLow;
+
+    public void clickOnPriceHighToLow() {
+        priceHighToLow.click();
+    }
+
+    public boolean priceIsInAscendingOrder() {
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        List<Integer> li = new ArrayList<>();
+
+        for (int i = 0; i < 8; i++) {
+            List<WebElement> priceList = driver.findElements(By.xpath("//android.widget.TextView[contains(@text,'Price')]/following-sibling::android.widget.TextView[1]"));
+            for (WebElement price : priceList) {
+                String p = price.getText().substring(12);
+
+                try {
+                    System.out.println(p.trim());
+                    li.add(Integer.parseInt(p.replaceAll("[^0-9]", "")));
+                } catch (NumberFormatException e) {
+                    System.out.println(e);
+                    System.err.println("Failed to parse integer from: " + p);
+                }
+
+            }
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            int w = driver.manage().window().getSize().getWidth();
+            int h = driver.manage().window().getSize().getHeight();
+            scrollOrSwipe(w / 2, h / 2 + h / 4, w / 2, 0);
+
+        }
+        System.out.println("Original list (low to high) \n" + li);
+
+        List<Integer> copy_li = new ArrayList<>(li);
+        System.out.println("Copy of original list(low to high) \n " + copy_li);
+
+        Collections.sort(copy_li);
+        System.out.println("Sorted list (low to high) \n" + copy_li);
+
+        return li.equals(copy_li);
+    }
+
+    @FindBy(xpath = "//android.widget.TextView[@text=\"Filter\"]")
+    WebElement filter;
+
+    public void clickOnFilter() {
+        filter.click();
+
+    }
+
+    @FindBy(xpath = "//android.view.View[contains(@content-desc,\"Free cancellation\")]")
+    WebElement freeCancellation;
+    @FindBy(xpath = "//android.widget.TextView[@text=\"Show results\"]")
+    WebElement showResults;
+
+    public void selectFreeCancellation() {
+        boolean flag = true;
+        while (flag) {
+
+            int w = driver.manage().window().getSize().getWidth();
+            int h = driver.manage().window().getSize().getHeight();
+            scrollOrSwipe(w / 2, h / 2 , w / 2, 0);
+
+            try {
+               freeCancellation.isDisplayed();
+                flag=false;
+            }catch (Exception e){
+                System.out.println("not displayed");
+            }
+
+        }
+        freeCancellation.click();
+        waitForElementToBeVisible(showResults);
+        showResults.click();
+    }
+
+    public boolean hotelsHaveFreeCancellationOption() {
+
+        List<String> li = new ArrayList<>();
+
+        for(int i = 0;i<6;i++) {
+
+            List<WebElement> cancellationList = driver.findElements(By.xpath("//android.widget.TextView[@text=\"��Free cancellation\"]"));
+            for (WebElement cancel : cancellationList) {
+                System.out.println(cancel.getText());
+                li.add(cancel.getText());
+            }
+            int w = driver.manage().window().getSize().getWidth();
+            int h = driver.manage().window().getSize().getHeight();
+            scrollOrSwipe(w / 2, h / 2 + h / 4, w / 2, 0);
+        }
+        boolean flag=true;
+        for (String l:li){
+            if(!l.contains("Free cancellation")){
+                System.out.println(l);
+                flag=false;
+                break;
+            }
+        }
+        return flag;
     }
 }
 
--- src/test/java/com/automation/steps/HotelSteps.java
+++ src/test/java/com/automation/steps/HotelSteps.java
@@ -68,4 +68,77 @@ public class HotelSteps {
     public void verifyUserIsOnHotelsIsAddedToWishlist() {
         Assert.assertTrue(hotelPage.verifyHotelIsInWishlist());
     }
+
+    @When("user clicks first hotel")
+    public void user_clicks_first_hotel() {
+        hotelPage.clickOnFirstHotel();
+    }
+    @Then("verify user is on hotel details page")
+    public void verify_user_is_on_hotel_details_page() {
+       Assert.assertTrue(hotelPage.hotelDetailsPage());
+    }
+    @When("user clicks on see availability button")
+    public void user_clicks_on_see_availability_button() {
+        hotelPage.clickOnAvailabilityBtn();
+    }
+    @Then("verify user is on reservation page")
+    public void verify_user_is_on_reservation_page() {
+        Assert.assertTrue(hotelPage.reservationPage());
+    }
+    @When("user select and customise the hotel")
+    public void user_select_and_customise_the_hotel() {
+
+        hotelPage.selectAndCustomiseHotel();
+    }
+    @When("user clicks on reserve button")
+    public void user_clicks_on_reserve_button() {
+        hotelPage.clickOnReserveBtn();
+    }
+    @Then("verify user is on fill information page")
+    public void verify_user_is_on_fill_information_page() {
+        Assert.assertTrue(hotelPage.fillInfoPage());
+    }
+    @When("fill the information")
+    public void fill_the_information() {
+        hotelPage.fillInfo();
+    }
+    @When("user clicks on next step")
+    public void user_clicks_on_next_step() {
+        hotelPage.clickOnNextStep();
+    }
+    @Then("Verify user is on booking overview page")
+    public void verify_user_is_on_booking_overview_page() {
+        Assert.assertTrue(hotelPage.bookingReviewPage());
+    }
+
+    @And("user clicks on sort")
+    public void userClicksOnSort() {
+        hotelPage.clickOnSort();
+    }
+
+    @And("user select price low to high sorting")
+    public void userSelectPriceLowToHighSorting() {
+        hotelPage.clickOnPriceHighToLow();
+    }
+
+    @Then("verify price is in ascending order")
+    public void verifyPriceIsInAscendingOrder() {
+        Assert.assertTrue(hotelPage.priceIsInAscendingOrder());
+    }
+
+    @And("user clicks on filter")
+    public void userClicksOnFilter() {
+        hotelPage.clickOnFilter();
+    }
+
+    @And("user select free cancellation")
+    public void userSelectFreeCancellation() {
+        hotelPage.selectFreeCancellation();
+
+    }
+
+    @Then("verify the displayed hotels have the option of free cancellation")
+    public void verifyTheDisplayedHotelsHaveTheOptionOfFreeCancellation() {
+        Assert.assertTrue(hotelPage.hotelsHaveFreeCancellationOption());
+    }
 }
